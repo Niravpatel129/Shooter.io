183e07708c21e7c571f22d81cf3c8da79ea67a8e
niravpatel129 <niravpatelp129@gmail.com>
2019-09-15 13:43:37 -0400

    updated host and port

--- public/js/sketch.js
+++ public/js/sketch.js
@@ -3,15 +3,16 @@ var socket = io();
 var player;
 var sprite;
 var spritesheet;
-
+var speed = 5;
+var serverPlayers;
 // inital
 localPlayer.x = 0;
 localPlayer.y = 0;
 
 localPlayer.xstart = 0;
 localPlayer.ystart = 0;
-localPlayer.skinHeight = 120;
-localPlayer.skinWidth = 120;
+localPlayer.skinHeight = 100;
+localPlayer.skinWidth = 100;
 
 socket.on("connect", () => {
   console.log("connection was sucessful");
@@ -22,35 +23,59 @@ function setup() {
   createCanvas(displayWidth, displayHeight);
 
   //load spritesheet
-  spritesheet = loadImage("./assets/spritesheet.png");
+  // spritesheet = loadImage("./assets/spritesheet.png");
 
   // make a new player object from player.js
   // set the data to it, possiblity hold the data locally?
 }
 
 function draw() {
+  //translate the scene with player movement keeping it centered
+  translate(width / 2 - localPlayer.x, height / 2.5 - localPlayer.y);
+
+  //check for keyboard+mouse input
+  checkIfMouseDown();
+
+  //draw temp terrain and background
   background(200);
+  fill(1000);
+  circle(30, 30, 200);
+
   // draw client player
-  image(
-    spritesheet,
-    localPlayer.x,
-    localPlayer.y,
-    (sWidth = 120),
-    (sHeight = 120),
-    0,
-    0,
-    100,
-    100
-  );
+  stroke(127, 63, 120);
+  fill(100, 40, 192, 127);
+  rect(localPlayer.x, localPlayer.y, 90, 90);
 
-  // draw server player
+  // if there is user on the server Draw them!
+  if (serverPlayers) {
+    fill(200, 120, 192, 127);
+    rect(serverPlayers.x, serverPlayers.y, 90, 90);
+  }
 
-  // draw server + client objects
-
-  // interactions --- should be tough :(
+  //emit constntly the local player data
+  socket.emit("playerData", localPlayer);
 }
 
 function mousePressed() {
   console.log("mouse pressed");
-  localPlayer.x += 10;
 }
+
+var checkIfMouseDown = () => {
+  if (keyIsDown(37)) {
+    localPlayer.x -= speed;
+  }
+  if (keyIsDown(38)) {
+    localPlayer.y -= speed;
+  }
+  if (keyIsDown(39)) {
+    localPlayer.x += speed;
+  }
+  if (keyIsDown(40)) {
+    localPlayer.y += speed;
+  }
+};
+
+socket.on("playerData", data => {
+  serverPlayers = data;
+  console.log(serverPlayers);
+});
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require("express");
 var app = express();
 var http = require("http").createServer(app);
 var io = require("socket.io")(http);
+var port = process.env.PORT || 3000;
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
@@ -10,8 +11,12 @@ app.get("/", function(req, res) {
 
 io.on("connection", function(socket) {
   console.log("a user connected");
+
+  socket.on("playerData", data => {
+    socket.broadcast.emit("playerData", data);
+  });
 });
 
-http.listen(3000, function() {
-  console.log("listening on *:3000");
+http.listen(port, function() {
+  console.log("listening on *:", port);
 });
