b9daafe99e768d1f2220660024d7fd3b9d5e394d
niravpatel129 <niravpatelp129@gmail.com>
2019-09-20 13:06:10 -0400

    added death messages

--- public/js/killMessages.js
+++ public/js/killMessages.js
@@ -1,17 +1,28 @@
-function showKillMessage(data) {
-  $(".killMessage").css("display", "block");
+function showKillMessage() {
+  if (deadPlayersMessage.length > 0) {
+    $(".killMessage").css("display", "block");
 
-  for (let i = 0; i < deadPlayersMessage.length; i++) {
-    let deadPlayersMessages = deadPlayersMessage[i].split(" ");
-    var x = $(".killMessage").html(
-      `${deadPlayersMessages[0]} <b style="color: red">KILLED</b> ${
-        deadPlayersMessages[1]
-      }`
-    );
-  }
+    for (let i = 0; i < deadPlayersMessage.length; i++) {
+      let playersInMessage = deadPlayersMessage[i].split(" ");
+      if (playersInMessage[0] === socket.id) {
+        playersInMessage[0] = "<b style='color: blue'>YOU</b>";
+      }
+      console.log(playersInMessage[1], socket.id);
 
-  setInterval(() => {
-    deadPlayersMessage.pop();
+      if (playersInMessage[1] === socket.id) {
+        playersInMessage[1] = "<b style='color: blue'>YOU</b>";
+      }
+      var x = $(".killMessage").html(
+        `${playersInMessage[0]} <b style="color: red">KILLED</b> ${
+          playersInMessage[1]
+        }`
+      );
+      setInterval(() => {
+        deadPlayersMessage.splice(i, 1);
+        showKillMessage();
+      }, 5000);
+    }
+  } else {
     $(".killMessage").css("display", "none");
-  }, 5000);
+  }
 }
