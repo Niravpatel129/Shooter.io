df4cdee5f0a1e4ab31f3324a2fede8351ac444ab
niravpatel129 <niravpatelp129@gmail.com>
2019-09-26 11:29:31 -0400

    dead player notification

--- public/js/killMessages.js
+++ public/js/killMessages.js
@@ -1,3 +1,9 @@
 function showKillMessage(data) {
-  console.log(data);
+  for (let i = 0; i < serverPlayers.length; i++) {
+    if (serverPlayers[i].id === data.deadPlayerSocketID) {
+      serverPlayers[i].x = -9999;
+      serverPlayers[i].y = -9999;
+      serverPlayers[i].alive = false;
+    }
+  }
 }
